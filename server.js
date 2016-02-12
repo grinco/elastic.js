3b13c16be1fd457a46871003cee5057fdec75d42
Vadim Grinco <vadim@redhat.com>
2016-02-12 23:31:21 +0100

    Express modules were not installed
    Signed-off-by: Vadim Grinco <vadim@redhat.com>

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ const express = require('express');
 // Constants
 const PORT = 8080;
 
+// Init synaptic
+var synaptic = require('synaptic');
+var Neuron = synaptic.Neuron,
+    Layer = synaptic.Layer,
+    Network = synaptic.Network,
+    Trainer = synaptic.Trainer,
+    Architect = synaptic.Architect;
+
 // App
 const app = express();
 app.get('/', function (req, res) {
